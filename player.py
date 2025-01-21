ae2130d18a38a6bde55241d949ef63e58b9c7455
Brian <brianbatz03@gmail.com>
2025-01-20 21:43:59 -0600

    Adjusting shoot speed

--- player.py
+++ player.py
@@ -1,13 +1,14 @@
 from shot import Shot
 from circleshape import CircleShape
 from pygame import Vector2, draw, key, K_a, K_d, K_w, K_s, K_SPACE
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED, PLAYER_SHOOT_COOLDOWN
 
 
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.shoot_timer = 0
 
     def draw(self, screen):
         draw.polygon(screen, "white", self.triangle(), 2)
@@ -21,6 +22,7 @@ class Player(CircleShape):
         return [a, b, c]
 
     def update(self, dt):
+        self.shoot_timer -= dt
         keys = key.get_pressed()
 
         if keys[K_w]:
@@ -42,5 +44,8 @@ class Player(CircleShape):
         self.position += forward * PLAYER_SPEED * dt
     
     def shoot(self):
+        if self.shoot_timer > 0:
+            return
+        self.shoot_timer = PLAYER_SHOOT_COOLDOWN
         shot = Shot(self.position.x, self.position.y)
         shot.velocity = Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
